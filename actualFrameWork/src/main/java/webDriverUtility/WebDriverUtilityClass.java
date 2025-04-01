217b96e35a3ae00ea0b5555d41bb4c1be2b4d5d7
veeresh-mala <veereshmala22@gmail.com>
2025-04-01 09:28:01 +0530

    Different packages are

---
+++ actualFrameWork/src/main/java/webDriverUtility/WebDriverUtilityClass.java
@@ -0,0 +1,5 @@
+package webDriverUtility;
+
+public class WebDriverUtilityClass {
+
+}
---
+++ actualFrameWork/src/test/java/createContactTests/ToCreateContact.java
@@ -0,0 +1,10 @@
+package createContactTests;
+
+public class ToCreateContact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
