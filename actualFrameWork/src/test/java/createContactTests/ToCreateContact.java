12b8ab970396cf602f9e3ded264c05eb386909b6
SRIKANTH <SRIKANTH@DESKTOP-G13II8U>
2025-04-01 10:15:51 +0530

    Puli-Srikanth hello

--- actualFrameWork/src/test/java/createContactTests/ToCreateContact.java
+++ actualFrameWork/src/test/java/createContactTests/ToCreateContact.java
@@ -3,8 +3,8 @@ package createContactTests;
 public class ToCreateContact {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		System.out.println("Create Contact Successfully");
 	}
 
 }
